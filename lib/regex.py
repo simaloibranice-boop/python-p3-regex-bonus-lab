e9de6a17b07c4d8ba5af4753f294c5ec693e5124
Branice Simaloi <simaloibranice@gmail.com>
2025-11-24 14:15:45 +0300

    completed lab

--- lib/regex.py
+++ lib/regex.py
@@ -1,5 +1,4 @@
 import re
 
-my_pattern = r""
-my_regex = re.compile(my_pattern)
-
+my_pattern = r"^[ \t]*([A-Za-z \t'?!\.,]*day[A-Za-z \t'?!\.,]*[\.?])[ \t]*$"
+my_regex = re.compile(my_pattern, re.MULTILINE)
